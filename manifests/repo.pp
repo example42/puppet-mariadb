12ef6e0b812d7a35adbf9f3261f450004f5f76ce
Oleg Lavrovsky <oleg.lavrovsky@centralway.com>
2014-08-25 12:45:29 +0200

    Fixed Debian MariaDB mirror

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -25,21 +25,19 @@ class mariadb::repo (
 
   $repo_base_url = $base_url ? {
     ''      => $::osfamily ? {
-      'RedHat' => 'http://yum.mariadb.org',
-      'Debian' => 'http://mirror.1000mbps.com/mariadb/repo',
+      'Debian' => 'http://mariadb.mirror.nucleus.be/repo',
     },
     default => $base_url,
   }
 
   $repo_distro = $::operatingsystem ? {
-    'RedHat'    => 'rhel',
     'LinuxMint' => 'ubuntu',
     default     => downcase($operatingsystem),
   }
 
   $repo_version = $mariadb::version ? {
     /^5/   => '5.5',
-    /^10/  => '10.0',
+    /^10/  => '10.1',
   }
 
   $repo_arch = $::architecture ? {
@@ -51,16 +49,6 @@ class mariadb::repo (
   $osver = split($::operatingsystemrelease, '[.]')
 
   case $::osfamily {
-    redhat: {
-      yumrepo { 'mariadb':
-        descr          => 'MariaDB',
-        enabled        => '1',
-        gpgcheck       => '1',
-        baseurl        => "${repo_base_url}/${repo_version}/${repo_distro}${osver[0]}-${repo_arch}",
-        gpgkey         => 'https://yum.mariadb.org/RPM-GPG-KEY-MariaDB',
-        before         => $mariadb::manage_config_file_require,
-      }
-    }
     debian: {
       apt::source { 'mariadb':
         location   => "${repo_base_url}/${repo_version}/${repo_distro}",
